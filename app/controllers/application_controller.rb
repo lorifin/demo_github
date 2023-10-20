8fad536b735a2266dc88fe692e70f9e51ee3ae3a
sk888 <skre888@gmail.com>
2023-10-20 22:14:26 +0200

    Modif Page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    def index
+        render text: 'Hello World'
+     end 
+     def index
+        render text: 'About'
+     end 
+     def index
+        render text: 'Help'
+     end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   get 'pages/about'
   get 'pages/help'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/about' => 'pages#about'
+  get '/help' => 'pages#help'
+  get '/home' => 'pages#home'
+  get '/how-it-works' => 'pages#how_it_works'
+
 end
